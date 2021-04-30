70ddf9ee513494f06e86e40c0d012434388b7672
DESKTOP-PBHAHUQ\David <david.liebemann@gmail.com>
2021-04-30 19:35:29 +0200

    Added base open scene script and base scenes

---
+++ Notfallkoffer/Assets/Notfallkoffer/_Scripts/UI/OpenScene.cs
@@ -0,0 +1,31 @@
+﻿using Lean.Gui;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Notfallkoffer._Scripts.UI
+{
+    [RequireComponent(typeof(LeanButton))]
+    public class OpenScene : MonoBehaviour
+    {
+        [SerializeField] private string sceneName;
+
+        [SerializeField] private LeanButton button;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            button.OnClick.AddListener(OnClick);
+        }
+
+        private void OnDestroy()
+        {
+            button.OnClick.RemoveListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
