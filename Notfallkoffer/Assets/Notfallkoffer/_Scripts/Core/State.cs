c6c2001190a345a4d7ce443563f50dc128bc63fc
David Liebemann <david@crystalmesh.de>
2021-05-01 00:52:07 +0200

    Current progress on breathing logic

---
+++ Notfallkoffer/Assets/Notfallkoffer/_Scripts/Core/State.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Notfallkoffer._Scripts.Core
+{
+    public abstract class State : MonoBehaviour
+    {
+        protected virtual void Awake()
+        {
+            enabled = false;
+        }
+
+        public abstract void Enter();
+
+        public abstract State OnStateUpdate(float deltaTime);
+        public abstract void Exit();
+    }
+}
