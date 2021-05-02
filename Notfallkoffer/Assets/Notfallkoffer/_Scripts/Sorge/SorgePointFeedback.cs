f14c0c240779f41b3f0264f6134f549fd4ab0860
David Liebemann <david@crystalmesh.de>
2021-05-02 14:47:30 +0200

    Added lighting effect on pupils when doing a good job

--- Notfallkoffer/Assets/Notfallkoffer/_Scripts/Sorge/SorgePointFeedback.cs
+++ Notfallkoffer/Assets/Notfallkoffer/_Scripts/Sorge/SorgePointFeedback.cs
@@ -7,6 +7,8 @@ namespace Notfallkoffer._Scripts.Sorge
     {
         [SerializeField] private StateDataReferencer data;
         [SerializeField] private ScaleRangeData pupilData;
+        [SerializeField] private ScaleRangeData pupilLightData;
+        [SerializeField] private AlphaData pupilAlphaData;
 
 
         private void Update()
@@ -14,6 +16,8 @@ namespace Notfallkoffer._Scripts.Sorge
             float pointScore = data.points.GetNormalizedPointScore();
             data.schnurrenSource.volume = pointScore;
             pupilData.ApplyScale(pointScore);
+            // pupilLightData.ApplyScale(pointScore);
+            pupilAlphaData.ApplyAlpha(pointScore);
         }
     }
 
@@ -23,13 +27,48 @@ namespace Notfallkoffer._Scripts.Sorge
         [SerializeField] public GameObject[] targets;
         [SerializeField] public Vector3 minScale;
         [SerializeField] public Vector3 maxScale;
+        [Range(0.0f, 0.9f)] [SerializeField] private float zeroPoint = 0.0f;
+
 
         public void ApplyScale(float alpha)
         {
-            Vector3 newScale = minScale + (maxScale - minScale) * alpha;
+            float scaleFactor = Mathf.Clamp01(alpha - zeroPoint) / (1.0f - zeroPoint);
+            Vector3 newScale = minScale + (maxScale - minScale) * scaleFactor;
             foreach (GameObject target in targets)
             {
                 target.transform.localScale = newScale;
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class AlphaData
+    {
+        [SerializeField] public GameObject[] targets;
+        [SerializeField] public float minAlpha;
+        [SerializeField] public float maxAlpha;
+        [Range(0.0f, 0.9f)] [SerializeField] private float zeroPoint = 0.0f;
+        [Range(0.1f, 1.0f)] [SerializeField] private float onePoint = .8f;
+
+
+        public void ApplyAlpha(float normalizedProgress)
+        {
+            float toOne = 1.0f - onePoint;
+            float scaleFactor = Mathf.Clamp01(normalizedProgress - zeroPoint) /
+                                Mathf.Max(1.0f - zeroPoint - toOne, 0.1f);
+            float newAlpha = minAlpha + (maxAlpha - minAlpha) * scaleFactor;
+
+            // Debug.Log(newAlpha);
+
+            foreach (GameObject target in targets)
+            {
+                var spriteRenderers = target.GetComponentsInChildren<SpriteRenderer>();
+                foreach (var renderer in spriteRenderers)
+                {
+                    Color newColor = renderer.color;
+                    newColor.a = newAlpha;
+                    renderer.color = newColor;
+                }
             }
         }
     }
